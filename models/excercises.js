c6098fafa556c4c656493e4f509f9157e51450b3
Lauren <l.cvengros@icloud.com>
2022-10-22 15:34:24 -0400

    created exercise model

---
+++ models/excercises.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Exercise extends Model {};
+
+Exercise.init(
+    {
+        id: {
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true, 
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: false,
+        },
+        reps: {
+            type: DataTypes.INTEGER,
+            allownull: false,
+        },
+        sets: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        routine_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'routine',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'exercise'
+    },
+);
+
+module.exports = Exercise;
+
